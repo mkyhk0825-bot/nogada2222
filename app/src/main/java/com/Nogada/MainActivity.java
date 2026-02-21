0656a6b5eabfc6364adfe036d728d96a619358e4
mkyhk0825 <mkyhk0825@gmail.com>
2026-02-22 02:38:17 +0900

    Initial commit

---
+++ app/src/main/java/com/Nogada/MainActivity.java
@@ -0,0 +1,28 @@
+package com.Nogada;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient()); // ⭐ 이것만 유지
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
